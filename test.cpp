f553f43eaf872b74344c5652dec745ff2201e774
Билык Макар <makar.mill@mail.ru>
2024-09-06 21:45:06 +0300

    new file

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int area(int a, int b){
+    return a * b;
+}
+int perimetr(int a, int b){
+    return (a + b) * 2;
+}
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << area(a, b) << endl;
+    cout << perimetr(a, b) << endl;
+}
